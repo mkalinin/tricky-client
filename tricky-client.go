b40a417280b74c002bf5ff9eb65d70857675caed
Mikhail Kalinin <kalinin128@gmail.com>
2015-05-21 01:10:11 +0600

    adds X11 textview and redirects data output to it

--- tricky-client.go
+++ tricky-client.go
@@ -6,7 +6,13 @@ import (
   "os"
 )
 
-func main() {
+import (
+  "github.com/mattn/go-gtk/gdk"
+  "github.com/mattn/go-gtk/glib"
+  "github.com/mattn/go-gtk/gtk"
+)
+
+func dataLoop(buffer *gtk.TextBuffer, iter *gtk.TextIter) {
   args := os.Args[1:]
 
   host := args[0]
@@ -30,6 +36,40 @@ func main() {
   for {
     msg  := reader.Read()
     data := decryptor.Decrypt(msg)
-    fmt.Print(string(data))
+    gdk.ThreadsEnter()
+    str := string(data)
+    buffer.Insert(iter, str)
+    fmt.Print(str)
+    gdk.ThreadsLeave()
   }
+}
+
+func main() {
+  glib.ThreadInit(nil)
+  gdk.ThreadsInit()
+  gdk.ThreadsEnter()
+  gtk.Init(nil)
+
+  window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
+  window.SetPosition(gtk.WIN_POS_CENTER)
+  window.SetTitle("Tricky Client")
+  window.Connect("destroy", gtk.MainQuit)
+
+  textview := gtk.NewTextView()
+  textview.SetEditable(true)
+  textview.SetCursorVisible(true)
+  textview.SetSizeRequest(600, 600)
+  textview.SetWrapMode(gtk.WRAP_WORD)
+  
+  var iter gtk.TextIter
+  buffer := textview.GetBuffer()
+  buffer.GetStartIter(&iter)
+
+  window.Add(textview)
+  window.SetSizeRequest(600, 600)
+  window.ShowAll()
+
+  go dataLoop(buffer, &iter)
+
+  gtk.Main()
 }
